dcb7019d65e2bea12a74ebc00c48b27450218e74
Asmita0915 <asmitamutgekar@gmail.com>
2017-08-16 21:01:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,17 @@
-
 var button=document.getElemmentById('counter');
-//var counter=0;
 
- button.onclick = fuction() {
+ button.onclick = function() {
     
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(requaet.readystate == XMLHttpRequest.DONE){
             if(request.status == 200){
-                var counter=response.responseText;
-                counter=counter+1;
+                var counter=request.responseText;
                var span=document.getElementById('count');
                span.innerHTML=counter.toString();
             }
         }
     };
-   
+   request.open('get','http://asmitamutgekar.imad.hasura-app.io/counter',true);
+   request.send(null);
 };
