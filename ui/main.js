d76bd2c8902281a85ce748e4a764989770607b44
Asmita0915 <asmitamutgekar@gmail.com>
2017-08-16 20:53:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
-console.log('Loaded!');
+
+var button=document.getElemmentById('counter');
+//var counter=0;
+
+ button.onclick = fuction() {
+    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(requaet.readystate == XMLHttpRequest.DONE){
+            if(request.status == 200){
+                var counter=response.responseText;
+                counter=counter+1;
+               var span=document.getElementById('count');
+               span.innerHTML=counter.toString();
+            }
+        }
+    };
+   
+};
