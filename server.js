b76c75627ddce5b1129ca53d449cf47c4f049607
Asmita0915 <asmitamutgekar@gmail.com>
2017-08-16 20:25:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,8 +74,14 @@ var htmltemplate =`
 `;
 return htmltemplate;
 }
+var counter=0;
 
 app.use(morgan('combined'));
+
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
