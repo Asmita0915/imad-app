42257cb7c757757bd81b3000474292a3a038f9d7
Asmita0915 <asmitamutgekar@gmail.com>
2017-08-17 21:45:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ var button=document.getElementById("counter");
                var counter=request.responseText;
                var span=document.getElementById("count");
                console.log(span);
-               span.innerHTML=counter.toString()
+               span.innerHTML=" "+counter.toString()
                console.log(counter);
             }
         }
