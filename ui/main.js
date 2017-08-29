d698a38500ce67a6385993bd3caff6622ed4d5c1
Asmita0915 <asmitamutgekar@gmail.com>
2017-08-29 21:27:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,5 +24,11 @@ var input=document.getElementById("name");
 var data=input.value;
 var submit=document.getElementById("submit_b");
 submit.onclick=function(){
-    
-}
+ var names=["name1","name2","name3","name4"];   
+ var list='';
+ for(var i=0;i<names.length;i++){
+     list+='<li>'+names[i]+'</li>';
+ }
+ var ul=document.getElementById("list");
+ ul.innerHTML=list;
+};
