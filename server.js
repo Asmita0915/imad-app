b93ad13816bacbdb7a9eb2f6753ca3c9ab935a9b
Asmita0915 <asmitamutgekar@gmail.com>
2017-08-29 21:47:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.listen(port, function () {
 var names=[];
 app.get('/submit-name',function(req,res){
    //var name=req.params.name;
-   var name=req.qurey.name;
+   var name=req.query.name;
     names.push(name);
     
     res.send(JSON.stringify(names));
