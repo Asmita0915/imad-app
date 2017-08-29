87096423b4d63ec91a8aec6b74f4683cf2f8994d
Asmita0915 <asmitamutgekar@gmail.com>
2017-08-29 22:08:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ request.onreadystatechange = function(){
         if(request.status === 200){
             
             var names=request.responseText;
-            names=JSON.parse(name);
+            names=JSON.parse(names);
             var list='';
             for(var i=0;i<names.length;i++){
                 list+='<li>'+names[i]+'</li>';
@@ -42,11 +42,11 @@ request.onreadystatechange = function(){
             var ul=document.getElementById("list");
             ul.innerHTML=list;
         }
-    };
+    }
+    
+};
     request.open('GET','http://asmitamutgekar.imad.hasura-app.io/submit-name?name='+data,true);
     request.send(null);
-}
-    
     
 
 };
